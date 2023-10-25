8d3d991f7a8fdaa404f8541695c184574b55f94a
willyishmael <wtangka22@gmail.com>
2023-10-25 15:37:41 +0800

    add tasks page

--- lib/application/routes/navigation_helper.dart
+++ lib/application/routes/navigation_helper.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:todo_app/application/pages/home/home_page.dart';
 import 'package:todo_app/application/pages/root/root_page.dart';
 import 'package:todo_app/application/pages/settings/settings_page.dart';
+import 'package:todo_app/application/pages/tasks/tasks_page.dart';
 import 'package:todo_app/application/routes/get_page.dart';
 import 'package:todo_app/application/routes/go_router_observer.dart';
 import 'package:todo_app/application/routes/paths.dart';
@@ -17,6 +18,8 @@ class NavigationHelper {
       GlobalKey<NavigatorState>(debugLabel: 'root');
   final GlobalKey<NavigatorState> homeNavigationKey =
       GlobalKey<NavigatorState>(debugLabel: 'home');
+  final GlobalKey<NavigatorState> tasksNavigationKey =
+      GlobalKey<NavigatorState>(debugLabel: 'tasks');
   final GlobalKey<NavigatorState> settingsNavigationKey =
       GlobalKey<NavigatorState>(debugLabel: 'settings');
 
@@ -41,6 +44,18 @@ class NavigationHelper {
                 path: Paths.homePath,
                 pageBuilder: (context, state) => getPage(
                   child: const HomePage(),
+                  state: state,
+                ),
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: tasksNavigationKey,
+            routes: [
+              GoRoute(
+                path: Paths.tasksPath,
+                pageBuilder: (context, state) => getPage(
+                  child: const TasksPage(),
                   state: state,
                 ),
               ),
--- lib/application/routes/paths.dart
+++ lib/application/routes/paths.dart
@@ -3,5 +3,6 @@ class Paths {
   // home
   static const String rootPath = '/root';
   static const String homePath = '$rootPath/home';
+  static const String tasksPath = '$rootPath/tasks';
   static const String settingsPath = '$rootPath/settings';
 }
