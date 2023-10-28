857648c0b859c2d6ffa6c4d83a9541fbc8d88ddb
willyishmael <wtangka22@gmail.com>
2023-10-28 17:37:04 +0800

    Update todo_color.dart

--- lib/domain/entities/todo_color.dart
+++ lib/domain/entities/todo_color.dart
@@ -4,14 +4,14 @@ class TodoColor {
   final int colorIndex;
 
   static const List<Color> predefinedColors = [
-    Colors.red,
-    Colors.blueGrey,
-    Colors.yellow,
-    Colors.blue,
-    Colors.green,
-    Colors.purple,
-    Colors.teal,
-    Colors.orange,
+    Color.fromARGB(255, 240, 160, 154),
+    Color.fromARGB(255, 139, 192, 219),
+    Color.fromARGB(255, 218, 210, 142),
+    Color.fromARGB(255, 186, 144, 226),
+    Color.fromARGB(255, 134, 216, 137),
+    Color.fromARGB(255, 228, 143, 190),
+    Color.fromARGB(255, 166, 223, 213),
+    Color.fromARGB(255, 230, 201, 158),
   ];
 
   Color get color => predefinedColors[colorIndex];
